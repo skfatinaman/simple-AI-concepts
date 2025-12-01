5e19836f6d855c0abdf18fe82139402ebae97e88
Fatin Aman <skfatinaman22@gmail.com>
2025-12-01 18:32:08 +0600

    added node count

--- informedSearch/A_star.py
+++ informedSearch/A_star.py
@@ -5,6 +5,7 @@ def aStartSearch(graph,heuristics, start, goal):
     minHeap = [(heuristics[start],0,start)]
     parent = {start : None}
     distance = {start : 0}
+    nodes_visited = 1
     while minHeap:
         goodness, cost, node = heapq.heappop(minHeap)
         if node == goal :
@@ -13,20 +14,23 @@ def aStartSearch(graph,heuristics, start, goal):
                 path.append(node)
                 node = parent[node]
             path.reverse()
-            return path, cost
+            return path, cost, nodes_visited
         for neighbor, newCost in graph[node].items():
             totalCost = cost + newCost
+            nodes_visited +=1
             if neighbor not in distance.keys() or distance[neighbor] > totalCost:
                 heapq.heappush(minHeap, (totalCost+heuristics[neighbor], totalCost, neighbor))
                 parent[neighbor] = node
                 distance[neighbor] = totalCost
-    return None, None
+    return None, None, nodes_visited
 
 graph = distanceGraph.graph
 start = "A"
 goal = "L"
 heuristics = distanceGraph.heuristicsFunction(distanceGraph.coordinates, goal)
 
-path, cost = aStartSearch(graph,heuristics, start, goal)
+path, cost, nodes = aStartSearch(graph,heuristics, start, goal)
+print("A star Algorithm Results : ")
 print(f"The path is : {path}")
 print(f"The cost is : {cost}")
+print(f"Nodes visited : {nodes}")
--- informedSearch/bestFirst.py
+++ informedSearch/bestFirst.py
@@ -4,9 +4,11 @@ def bestFirstSearch(graph,heuristics, start, goal):
     path = [start]
     total_cost = 0
     node = start
+    nodesVisited = 1
     while node != goal:
         bestOptionFromThatNode = None
         for neighbor,cost in graph[node].items():
+            nodesVisited +=1
             if bestOptionFromThatNode == None :
                 bestOptionFromThatNode = neighbor
             else :
@@ -15,14 +17,16 @@ def bestFirstSearch(graph,heuristics, start, goal):
         total_cost += graph[node][bestOptionFromThatNode]
         node = bestOptionFromThatNode
         path.append(node)
-    return path, total_cost
+    return path, total_cost, nodesVisited
 
 graph = distanceGraph.graph
 start = "A"
 goal = "L"
 heuristics = distanceGraph.heuristicsFunction(distanceGraph.coordinates, goal)
 
-path, cost = bestFirstSearch(graph,heuristics, start, goal)
+path, cost, nodes = bestFirstSearch(graph,heuristics, start, goal)
 
+print("Best First Search Algorithm Results : ")
 print(f"The path is : {path}")
 print(f"The cost is : {cost}")
+print(f"Nodes visited : {nodes}")
